0ea126be1d745ebeb6780ee6b8e35f58909ebf61
userdyaaa <nadyanadhirsoraya@gmail.com>
2025-09-29 10:47:28 +0800

    Add files via upload

---
+++ Python/PRAK002-2510817120022-Nadya Nadhir Soraya.py
@@ -0,0 +1,3 @@
+print("Selamat Pagi,  Nadya Nadhir Soraya")
+print("Selamat Siang, Nadya Nadhir Soraya")
+print("Selamat Malam, Nadya Nadhir Soraya")
---
+++ Python/PRAK005-2510817120022-Nadya Nadhir Soraya.py
@@ -0,0 +1,8 @@
+# nama = "Nadya"
+print("Tampilkan Huruf Pertama: N\nTampilkan Huruf Terakhir: A")
+print("####        ###         #######    ")
+print("### ##      ###       ###     ###  ")
+print("###  ##     ###       ###########  ")
+print("###    ##   ###       ###     ###  ")
+print("###      ## ###       ###     ###  ")
+print("###       #####       ###     ###  ")
